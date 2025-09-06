470b131d2cd0b6bd62d798b447702108f57245ce
Dharmik2005-Godalka <dharmikgodalka@gmail.com>
2025-09-06 20:03:59 +0530

    Setup_File
    After main script, this is the my Setup file...

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Signal_ICT_Dharmik_92400133054",  # package folder name
+    version="1.0.0",
+    author="Dharmik Godalka",
+    description="Signal processing package for ICT assignment",
+    packages=find_packages(),
+    install_requires=["numpy","matplotlib"],
+)
